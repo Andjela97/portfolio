7c05369d1699bc5ac941ed03364487f706031815
Andjela97 <andjelajacimovic97@gmail.com>
2020-10-30 14:12:40 +0100

    hamburger menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,20 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  toggle() {
+    const toggleBtn = document.getElementById('toggleBtn');
+    const sideBar = document.getElementById('sidebar');
+
+    toggleBtn.classList.toggle('active');
+    sideBar.classList.toggle('active');
+  }
+
+
   toHome() {
     document.getElementById("home").scrollIntoView({ behavior: "smooth" });
+    const toggleBtn = document.getElementById('toggleBtn');
+    const sideBar = document.getElementById('sidebar');
+
+    toggleBtn.classList.remove('active');
+    sideBar.classList.remove('active');
   }
+
 
   toAbout() {
     document.getElementById("about").scrollIntoView({ behavior: "smooth" });
+    const toggleBtn = document.getElementById('toggleBtn');
+    const sideBar = document.getElementById('sidebar');
+
+    toggleBtn.classList.remove('active');
+    sideBar.classList.remove('active');
   }
 
   toProjects() {
     document.getElementById("projects").scrollIntoView({ behavior: "smooth" });
+    const toggleBtn = document.getElementById('toggleBtn');
+    const sideBar = document.getElementById('sidebar');
+
+    toggleBtn.classList.remove('active');
+    sideBar.classList.remove('active');
   }
 
   toContact() {
     document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+    const toggleBtn = document.getElementById('toggleBtn');
+    const sideBar = document.getElementById('sidebar');
+
+    toggleBtn.classList.remove('active');
+    sideBar.classList.remove('active');
   }
 
   constructor() { }
